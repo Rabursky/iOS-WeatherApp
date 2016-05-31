40403cb38de9306690aea13a55f05c74104244d3
Rabursky <rabursky@gmail.com>
2016-05-31 17:20:22 +0100

    UI test with Location Dialog fixed

--- CleanWeatherUITests/CleanWeatherUITests.swift
+++ CleanWeatherUITests/CleanWeatherUITests.swift
@@ -29,8 +29,11 @@ class CleanWeatherUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        self.addUIInterruptionMonitorWithDescription("Location Dialog") { (alert) -> Bool in
+            alert.buttons["Allow"].tap()
+            return true
+        }
+        XCUIApplication().buttons["reload"].tap()
     }
     
 }
